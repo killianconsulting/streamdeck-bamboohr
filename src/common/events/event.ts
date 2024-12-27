7c7f95af40aa9e5cf6c4624f1a965cb655bdb4fc
Bailey Killian <consulting@baileykillian.com>
2024-12-27 14:19:50 -0500

    Add files via upload

---
+++ src/common/events/event.ts
@@ -0,0 +1,19 @@
+import type { PluginEvent, UIEvent } from "../../api";
+
+/**
+ * Provides information for events received from Stream Deck.
+ */
+export class Event<T extends PluginEvent | UIEvent> {
+	/**
+	 * Event that occurred.
+	 */
+	public readonly type: T["event"];
+
+	/**
+	 * Initializes a new instance of the {@link Event} class.
+	 * @param source Source of the event, i.e. the original message from Stream Deck.
+	 */
+	constructor(source: T) {
+		this.type = source.event;
+	}
+}
